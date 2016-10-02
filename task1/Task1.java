858ba4decf9934e260371289c8204b44b91529c4
Nastya <rahonskaya.n@gmail.com>
2016-10-02 22:16:48 +0300

    add task1

---
+++ task1/Task1.java
@@ -0,0 +1,12 @@
+package task1;
+
+/**
+ * Created by HP on 02.10.2016.
+ */
+public class Task1 {
+  public static void main(String[] args) {
+    for (int i = args.length; i>=0; i--) {
+      System.out.println("Argument" + i + " = " + args[i]);
+    }
+  }
+}
