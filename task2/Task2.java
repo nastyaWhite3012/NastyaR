6c4781303c9c9faa9f34e837733373092a127107
Nastya <rahonskaya.n@gmail.com>
2016-10-03 16:10:32 +0300

    v1.2

--- task2/Task2.java
+++ task2/Task2.java
@@ -1,14 +1,14 @@
-package task2;
-
-import java.util.Scanner;
-
-/**
- * Created by HP on 02.10.2016.
- */
 public class Task2 {
+  /**
+   * Class Task2
+   */
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in); //Create an object scanner of the class Scanner
-    String name = scanner.nextLine(); //Read input data
-    System.out.println("Hello, " + name);
+    if(args.length !=0) {
+      for (int i=0; i<args.length; i++) {
+        System.out.println("Hello, " + args[i]); //Output
+      }
+    }
+    else
+      System.out.println("Wrong data!");
   }
 }
