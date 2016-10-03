a2160fd6a580c4db68aa8d5346fb844982865c8c
Nastya <rahonskaya.n@gmail.com>
2016-10-03 16:13:27 +0300

    v1.2

--- task3/Main.java
+++ task3/Main.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
-
-/**
- * Created by HP on 02.10.2016.
- */
 public class Main {
+  /**
+   * Class Main
+   */
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in); //Create an object scanner in the class Scanner
     Calculator calc = new Calculator(); //Create an object calk in the class Calculator
     System.out.println("Enter 2 numbers.");
-    double number1 = scanner.nextDouble(); //Read input data
-    double number2 = scanner.nextDouble(); //Read input data
+    double number1 = Double.parseDouble(args[0]);
+    double number2 = Double.parseDouble(args[1]);
     System.out.println("The sum of the numbers is " + calc.sum(number1, number2)); //Output the sum
     System.out.println("The difference of the numbers is " + calc.dif(number1, number2)); //Output the difference
-    System.out.println("The multiplication of the numbers is " + calc.mult(number1, number2)); //Output the multiplication
-    System.out.println("The division of the numbers is " + calc.div(number1, number2)); //Output the division
+    System.out.println("The multiplication of the numbers is " + calc.multiply(number1, number2)); //Output the multiplication
+    if (calc.check(number1, number2) == 0)
+      System.out.println("The division of the numbers is " + calc.div(number1, number2)); //Output the division
+    else
+      System.out.println("The division can't be done! Wrong data!");
 
   }
 }
