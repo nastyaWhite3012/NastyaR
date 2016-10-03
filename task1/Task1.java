530e93cd2be59ba9f84cb1b8cb556f39a15eecd9
Nastya <rahonskaya.n@gmail.com>
2016-10-03 16:05:08 +0300

    v1.2

--- task1/Task1.java
+++ task1/Task1.java
@@ -1,11 +1,9 @@
-package task1;
-
-/**
- * Created by HP on 02.10.2016.
- */
 public class Task1 {
+  /**
+   * Class Task1
+   */
   public static void main(String[] args) {
-    for (int i = args.length; i>=0; i--) {
+    for (int i = args.length-1; i>=0; i--) {
       System.out.println("Argument" + i + " = " + args[i]); //Output elements in reverse order
     }
   }
