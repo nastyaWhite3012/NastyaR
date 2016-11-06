a75eb820e84792748609b93b4afd561b809b4ddb
Nastya <rahonskaya.n@gmail.com>
2016-11-06 22:54:50 +0300

    v1.2

--- task14/TriangleTest.java
+++ task14/TriangleTest.java
@@ -5,9 +5,9 @@ import static org.testng.Assert.*;
 
 public class TriangleTest {
 
-  @DataProvider(name="DataProviderIsExist")
+  @DataProvider(name = "DataProviderIsExist")
   public Object[][] isExist() {
-    return new Object[][] {
+    return new Object[][]{
         {true, new Double(7.0), new Double(8.0), new Double(9.0)},
         {false, new Double(0.0), new Double(8.0), new Double(9.0)},
         {false, new Double(7.0), new Double(0.0), new Double(9.0)},
@@ -15,30 +15,30 @@ public class TriangleTest {
         {false, new Double(-7.0), new Double(8.0), new Double(9.0)},
         {false, new Double(7.0), new Double(-8.0), new Double(9.0)},
         {false, new Double(7.0), new Double(8.0), new Double(-9.0)},
-        {true, new Double(7.0/1.0), new Double(8.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0/1.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0), new Double(9.0/1.0)},
-        {true, new Double(7.0*1.0), new Double(8.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0*1.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0), new Double(9.0*1.0)},
-        {true, new Double(7.0+1.0), new Double(8.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0+1.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0), new Double(9.0+1.0)},
-        {true, new Double(7.0-2.0), new Double(8.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0-2.0), new Double(9.0)},
-        {true, new Double(7.0), new Double(8.0), new Double(9.0-3.0)},
-        {false, new Double(7.0/7.0), new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0/8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), new Double(9.0/9.0)},
-        {false, new Double(7.0*7.0), new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0*8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), new Double(9.0*9.0)},
-        {false, new Double(7.0+10.0), new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0+8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), new Double(9.0+9.0)},
-        {false, new Double(7.0-7.0), new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0-8.0), new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), new Double(9.0-9.0)},
+        {true, new Double(7.0 / 1.0), new Double(8.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0 / 1.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0), new Double(9.0 / 1.0)},
+        {true, new Double(7.0 * 1.0), new Double(8.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0 * 1.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0), new Double(9.0 * 1.0)},
+        {true, new Double(7.0 + 1.0), new Double(8.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0 + 1.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0), new Double(9.0 + 1.0)},
+        {true, new Double(7.0 - 2.0), new Double(8.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0 - 2.0), new Double(9.0)},
+        {true, new Double(7.0), new Double(8.0), new Double(9.0 - 3.0)},
+        {false, new Double(7.0 / 7.0), new Double(8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0 / 8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0), new Double(9.0 / 9.0)},
+        {false, new Double(7.0 * 7.0), new Double(8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0 * 8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0), new Double(9.0 * 9.0)},
+        {false, new Double(7.0 + 10.0), new Double(8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0 + 8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0), new Double(9.0 + 9.0)},
+        {false, new Double(7.0 - 7.0), new Double(8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0 - 8.0), new Double(9.0)},
+        {false, new Double(7.0), new Double(8.0), new Double(9.0 - 9.0)},
         {false, Double.NaN, new Double(8.0), new Double(9.0)},
         {false, new Double(7.0), Double.NaN, new Double(9.0)},
         {false, new Double(7.0), new Double(8.0), Double.NaN},
@@ -48,24 +48,20 @@ public class TriangleTest {
         {false, Double.NEGATIVE_INFINITY, new Double(8.0), new Double(9.0)},
         {false, new Double(7.0), Double.NEGATIVE_INFINITY, new Double(9.0)},
         {false, new Double(7.0), new Double(8.0), Double.NEGATIVE_INFINITY},
-        {false, Double.MIN_VALUE, new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), Double.MIN_VALUE, new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), Double.MIN_VALUE},
-        {false, Double.MAX_VALUE, new Double(8.0), new Double(9.0)},
-        {false, new Double(7.0), Double.MAX_VALUE, new Double(9.0)},
-        {false, new Double(7.0), new Double(8.0), Double.MAX_VALUE},
+        {true, Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
+        {true, Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE},
     };
   }
 
   @Test(dataProvider = "DataProviderIsExist")
-  public void testIsExist(boolean expected, double a, double b, double c){
+  public void testIsExist(boolean expected, double a, double b, double c) {
     Triangle triangle = new Triangle();
     assertEquals(expected, triangle.isExist(a, b, c));
   }
 
-  @DataProvider(name="DataProviderIsEquilateral")
+  @DataProvider(name = "DataProviderIsEquilateral")
   public Object[][] isEquilateral() {
-    return new Object[][] {
+    return new Object[][]{
         {true, new Double(7.0), new Double(7.0), new Double(7.0)},
         {false, new Double(7.0), new Double(7.0), new Double(8.0)},
         {false, new Double(7.0), new Double(8.0), new Double(9.0)},
@@ -73,14 +69,14 @@ public class TriangleTest {
   }
 
   @Test(dataProvider = "DataProviderIsEquilateral")
-  public void testIsEquilateral(boolean expected, double a, double b, double c){
+  public void testIsEquilateral(boolean expected, double a, double b, double c) {
     Triangle triangle = new Triangle();
     assertEquals(expected, triangle.isEquilateral(a, b, c));
   }
 
-  @DataProvider(name="DataProviderIsIsosceles")
+  @DataProvider(name = "DataProviderIsIsosceles")
   public Object[][] isIsosceles() {
-    return new Object[][] {
+    return new Object[][]{
         {false, new Double(7.0), new Double(7.0), new Double(7.0)},
         {true, new Double(7.0), new Double(7.0), new Double(8.0)},
         {false, new Double(7.0), new Double(8.0), new Double(9.0)},
@@ -89,7 +85,7 @@ public class TriangleTest {
 
   @Test(dataProvider = "DataProviderIsIsosceles")
   public void testIsIsosceles(boolean expected, double a, double b, double c) {
-    Triangle triangle  = new Triangle();
+    Triangle triangle = new Triangle();
     assertEquals(expected, triangle.IsIsosceles(a, b, c));
   }
 }
