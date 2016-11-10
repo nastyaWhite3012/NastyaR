976eafe9e5b6a46fe5e33ef941ccee65c7f6b228
Nastya <rahonskaya.n@gmail.com>
2016-11-10 15:00:19 +0300

    add changes in ddt

--- task14/TriangleTest.java
+++ task14/TriangleTest.java
@@ -1,12 +1,16 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 import static org.testng.Assert.*;
@@ -16,19 +20,28 @@ public class TriangleTest {
   public static final String PATH = ".\\ddt.xml";
   private static final String NEGATIVE = "negative";
   private static final String POSITIVE = "positive";
-  private static final String NULLSIDE = "nullSide";
   private static final String SIDE_A = "side_a";
   private static final String SIDE_B = "side_b";
   private static final String SIDE_C = "side_c";
   private static final String EXPECTED = "expected";
 
-  @DataProvider(name = "negativeFromXml")
-  public Object[][] readNegativeXML() throws Exception {
+  Document document;
+
+  @BeforeTest
+  public Document setUp() throws IOException, SAXException, ParserConfigurationException {
     File inputFile = new File(PATH);
     DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     DocumentBuilder builder = factory.newDocumentBuilder();
+    document = builder.parse(inputFile);
+    return document;
+  }
 
-    Document document = builder.parse(inputFile);
+  @DataProvider(name = "negativeFromXml")
+  public Object[][] readNegativeXML() throws Exception {
+//    File inputFile = new File(PATH);
+//    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//    DocumentBuilder builder = factory.newDocumentBuilder();
+//    Document document = builder.parse(inputFile);
     NodeList nodes = document.getElementsByTagName(NEGATIVE);
     Object[][] result = new Object[nodes.getLength()][];
     for (int i = 0; i < nodes.getLength(); i++) {
@@ -51,10 +64,10 @@ public class TriangleTest {
 
   @DataProvider(name = "positiveFromXml")
   public Object[][] readXMLPositive() throws Exception {
-    File inputFile = new File(PATH);
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder builder = factory.newDocumentBuilder();
-    Document document = builder.parse(inputFile);
+//    File inputFile = new File(PATH);
+//    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//    DocumentBuilder builder = factory.newDocumentBuilder();
+//    Document document = builder.parse(inputFile);
     NodeList nodes = document.getElementsByTagName(POSITIVE);
     Object[][] result = new Object[nodes.getLength()][];
     for (int i = 0; i < nodes.getLength(); i++) {
@@ -70,7 +83,7 @@ public class TriangleTest {
   }
 
   @Test(dataProvider = "positiveFromXml")
-  public void testIsExistPositive(boolean expected, BigDecimal a, BigDecimal b, BigDecimal c) {
+  public void testIsExist(boolean expected, BigDecimal a, BigDecimal b, BigDecimal c) {
     Triangle triangle = new Triangle();
     assertEquals(expected, triangle.isExist(a, b, c));
   }
